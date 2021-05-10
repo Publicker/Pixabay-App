fb15a6aa29dfc2476aca58fdc4c1b67f08a82d15
Pablo Maldonado <public.maldonado@gmail.com>
2021-05-09 22:36:12 -0300

    🔐 Added config file for credentials

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+const config = {
+  pixabay: {
+    // IMPORTANT: In order to use Pixabay's api you need a Pixabay's api key
+    // (https://pixabay.com/api/docs/)
+    apiKey: "",
+  },
+};
+
+export default config;
